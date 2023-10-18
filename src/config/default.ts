9450bd6c3d7e80ccb41b0ca1e3d843d6cb8e80b5
Kelvin Omozokpia <57039338+omozokelvin@users.noreply.github.com>
2023-10-18 21:54:21 +0100

    updated default.ts config

--- src/config/default.ts
+++ src/config/default.ts
@@ -1,7 +1,7 @@
 export default {
   port: process.env.PORT,
-  dbUri: String(process.env.DBURI),
-  saltWorkFactor: parseInt(process.env.SALTWORKFACTOR ?? '10'),
+  dbUri: process.env.DBURI,
+  saltWorkFactor: +(process.env.SALTWORKFACTOR || 10),
   accessTokenTtl: process.env.ACCESS_TOKEN_TTL,
   refreshTokenTtl: process.env.REFRESH_TOKEN_TTL,
   mailingPassword: process.env.MAILING_PASSWORD,
