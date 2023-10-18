d3dc35c43a63373b1c778c8ad233054979c4c07a
Kelvin Omozokpia <57039338+omozokelvin@users.noreply.github.com>
2023-10-18 21:38:25 +0100

    Config done properly

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,20 @@
-import "dotenv/config";
+import 'dotenv/config';
+
+process.env['NODE_CONFIG_DIR'] = __dirname + '/config';
+
+import config from 'config';
 import express from 'express';
-import config from 'config'
-import logger from './utils/logger';
-import connect from './utils/connect';
 import routes from './routes';
+import connect from './utils/connect';
+import logger from './utils/logger';
 
-
-
-const port = config.get<number>("port");
+const port = config.get<number>('port');
 const app = express();
 
 app.use(express.json());
 
-
 app.listen(port, async () => {
-    logger.info(`App is running at http://localhost:${port}`)
-    await connect();
-    routes(app);
-})
-
-
-
+  logger.info(`App is running at http://localhost:${port}`);
+  await connect();
+  routes(app);
+});
--- src/config/default.ts
+++ src/config/default.ts
@@ -1,16 +1,10 @@
-
-
-console.log(process.env)
-
-
 export default {
-    port: process.env.PORT,
-    dbUri: String(process.env.DBURI),
-    saltWorkFactor: parseInt(process.env.SALTWORKFACTOR ?? "10"),
-    accessTokenTtl: process.env.ACCESS_TOKEN_TTL,
-    refreshTokenTtl: process.env.REFRESH_TOKEN_TTL,
-    mailingPassword: process.env.MAILING_PASSWORD,
-    publicKey: process.env.PUBLIC_KEY,
-    privateKey: process.env.PRIVATE_KEY,
+  port: process.env.PORT,
+  dbUri: String(process.env.DBURI),
+  saltWorkFactor: parseInt(process.env.SALTWORKFACTOR ?? '10'),
+  accessTokenTtl: process.env.ACCESS_TOKEN_TTL,
+  refreshTokenTtl: process.env.REFRESH_TOKEN_TTL,
+  mailingPassword: process.env.MAILING_PASSWORD,
+  publicKey: process.env.PUBLIC_KEY,
+  privateKey: process.env.PRIVATE_KEY,
 };
-
